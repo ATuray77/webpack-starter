8d5253c6b546b1a16c2d0f808161972f56c54aa2
Aruna Turay <tura_arun@yahoo.com>
2024-01-25 12:55:13 -0500

    completing webpack

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// import { readFile } from "node:fs/promises";
+// import fsPromises from "node:fs/promises";
+
+// Correct import for Node.js 10.0.0 and later
+//const { promises: fsPromises } = require('fs');
+//const promises = require('fs');
+
+import message from "./message";
+import './css/style.css';
+
+console.log(message)
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path'); //path module from node.js. Allows us to use absolute path name
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',  //source
+    output: {
+        path: path.resolve(__dirname, 'dist'), //allows us to use absolute path. Using dist folder as a directory
+        filename: 'bundle.js',
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist') //serving from here
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,  //means any file that ends with .css
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.js$/,  //means any file that ends with js
+                exclude: /node_modules/, //prevent it from looking in node modules
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: './src/index.html',
+        }),
+    ],
+};
