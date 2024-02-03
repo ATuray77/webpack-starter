7d3f1a1b2236e0f5509562b37a11111155973e3a
Aruna Turay <tura_arun@yahoo.com>
2024-02-03 10:33:12 -0500

    adds all recent changes

---
+++ src/components/Modal.js
@@ -0,0 +1,6 @@
+class Modal {
+    constructor() {
+        this.modal = document.querySelector('#modal') //bringing in the modal
+        this.modalBtn = document.querySelector('#modal-btn') //bringing in the modal btn
+    }
+}
--- src/index.js
+++ src/index.js
@@ -6,5 +6,23 @@
 //const promises = require('fs');
 
 import './css/style.css';
+import '@fortawesome/fontawesome-free/css/all.css'
 
 
+
+function open() { //when button is clicked we want the modal to display
+   modal.style.display = 'block';
+}
+
+function close() {
+    modal.style.display = 'none';
+}
+
+function outsideClick(e) {
+    if (e.target === modal) { //clicking the dark area of the modal will close it
+        close();
+    }
+}
+
+modalBtn.addEventListener('click', open)//event listener on the modal btn, with the open function
+window.addEventListener('click', outsideClick);//clicking outside the modal should close it
